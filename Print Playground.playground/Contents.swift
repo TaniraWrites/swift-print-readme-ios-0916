5febcd25b439a175fd6fb3f2f3f4273691072a92
Taniraw <tanirawrites@gmail.com>
2016-08-30 10:22:25 -0400

    String and Print Completed

---
+++ Print Playground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var favoriteWord = "Blah"
+var favoriteColor = "Navy Blue"
+var favoriteMusician = "Jimi Hendrix"
+
+let color = "blue"
+print("The sky is \(color).")
+
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
